07ca0efb8395abad40a9089a9c3900e39509909d
Thawane <thawaneapoliveira@gmail.com>
2022-07-26 19:54:17 -0300

    Adiciona requisito 10

--- src/challenges.js
+++ src/challenges.js
@@ -78,8 +78,18 @@ function decode() {
 }
 
 // // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tecnologia, nome) {
+  tecnologia.sort();
+  if (tecnologia.length <= 0) {
+    return 'Vazio!';
+  }
+  let objeto = [];
+  for (let index = 0; index < tecnologia.length; index += 1) {
+    objeto.push({
+      tech: tecnologia[index],
+      name: nome});
+  } 
+  return objeto
 }
 
 module.exports = {
