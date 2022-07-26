717515116f709814537e4a355c9abf755d83c180
Thawane <thawaneapoliveira@gmail.com>
2022-07-26 20:48:40 -0300

    Adiciona requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -64,8 +64,20 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let arrayComString = [];
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] % 3 === 0 && array[index] % 5 === 0) {
+      arrayComString.push('fizzBuzz');
+    } else if (array[index] % 3 === 0 && array[index] % 5 !== 0) {
+      arrayComString.push('fizz');
+    } else if (array[index] % 3 !== 0 && array[index] % 5 === 0) {
+      arrayComString.push('buzz');
+    } else {
+      arrayComString.push('bug!');
+    }
+  }
+  return arrayComString;
 }
 
 // // Desafio 9
