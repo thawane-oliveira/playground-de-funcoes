1d46709aa9386cdf4d9a38a5cb50121548cb5f2b
Thawane <thawaneapoliveira@gmail.com>
2022-07-26 15:19:23 -0300

    Adiciona requisito 02

--- src/challenges.js
+++ src/challenges.js
@@ -1,67 +1,76 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-  teste
-}
+// const girafa = true;
+// const elefante = true;
+// const macaco = false;
 
-// Desafio 2
-function calcArea() {
-  // seu código aqui
-}
+// function compareTrue(animal1, animal2) {
+//   if (animal1, animal2 === false && animal1 & animal2 !== true) {
+//     return false;
+//   } console.log(false);
+// }
 
-// Desafio 3
-function splitSentence() {
-  // seu código aqui
-}
+// // Desafio 2
 
-// Desafio 4
-function concatName() {
-  // seu código aqui
-}
+let area = 0;
 
-// Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+function calcArea(base, height) {
+  let area = base * height / 2; 
+  return area;
+} 
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
-}
+// // Desafio 3
+// function splitSentence() {
+//   // seu código aqui
+// }
 
-// Desafio 7
-function catAndMouse() {
-  // seu código aqui
-}
+// // Desafio 4
+// function concatName() {
+//   // seu código aqui
+// }
 
-// Desafio 8
-function fizzBuzz() {
-  // seu código aqui
-}
+// // Desafio 5
+// function footballPoints() {
+//   // seu código aqui
+// }
 
-// Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+// // Desafio 6
+// function highestCount() {
+//   // seu código aqui
+// }
 
-// Desafio 10
-function techList() {
-  // seu código aqui
-}
+// // Desafio 7
+// function catAndMouse() {
+//   // seu código aqui
+// }
+
+// // Desafio 8
+// function fizzBuzz() {
+//   // seu código aqui
+// }
+
+// // Desafio 9
+// function encode() {
+//   // seu código aqui
+// }
+// function decode() {
+//   // seu código aqui
+// }
+
+// // Desafio 10
+// function techList() {
+//   // seu código aqui
+// }
 
 module.exports = {
   calcArea,
-  catAndMouse,
-  compareTrue,
-  concatName,
-  decode,
-  encode,
-  fizzBuzz,
-  footballPoints,
-  highestCount,
-  splitSentence,
-  techList,
+//   catAndMouse,
+//   compareTrue,
+//   concatName,
+//   decode,
+//   encode,
+//   fizzBuzz,
+//   footballPoints,
+//   highestCount,
+//   splitSentence,
+//   techList,
 };
