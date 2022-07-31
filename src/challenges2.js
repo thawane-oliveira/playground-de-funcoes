18063b4cd297d9e8997935d16fe5a5a95e7f4c7a
Thawane <thawaneapoliveira@gmail.com>
2022-07-31 18:46:55 -0300

    Adiciona requisito bônus 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -31,8 +31,18 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let sugestao = string.split(/(\d)/);
+  let contagem = 0;
+  for (let index = 0; index < sugestao.length; index += 1) {
+    if (sugestao[index].length <= 1) {
+      let copo = Number(sugestao[index]);
+      contagem += copo;
+    }
+  }
+  if (contagem === 1) {
+    return `${contagem} copo de água`;
+  } return `${contagem} copos de água`;
 }
 
 module.exports = {
